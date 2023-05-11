d3a6cd90b6398f90a3ef9b6d3cd46d4600e0355e
jiwoo1087 <kingjiwoo1087@gmail.com>
2023-05-11 19:28:54 +0900

    계산기 완성

--- app/src/main/java/kr/hs/emirim/kjw/simplecalculator/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/kjw/simplecalculator/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var edit1 : EditText;
     lateinit var edit2 : EditText
     lateinit var btnAdd : Button;
-    lateinit var btnSub : Button
+    lateinit var btnMin : Button
     lateinit var btnMul : Button;
     lateinit var btnDiv : Button
     lateinit var textResult : TextView
@@ -20,5 +20,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        edit1 = findViewById<EditText>(R.id.edit1)
+        edit2 = findViewById<EditText>(R.id.edit2)
+        textResult = findViewById(R.id.text_result)
+        btnAdd = findViewById(R.id.btn_plus)
+        btnMin = findViewById(R.id.btn_minus)
+        btnMul = findViewById(R.id.btn_multiply)
+        btnDiv = findViewById(R.id.btn_dvide)
+        btnAdd.setOnClickListener {
+            num1 = edit1.text.toString()
+            num2 = edit2.text.toString()
+            result = Integer.parseInt(num1) + Integer.parseInt(num2)
+            textResult.text = "계산 결과: "+result
+        }
+        btnMin.setOnClickListener {
+            num1 = edit1.text.toString()
+            num2 = edit2.text.toString()
+            result = Integer.parseInt(num1) - Integer.parseInt(num2)
+            textResult.text = "계산 결과: "+result
+        }
+        btnMul.setOnClickListener {
+            num1 = edit1.text.toString()
+            num2 = edit2.text.toString()
+            result = Integer.parseInt(num1) * Integer.parseInt(num2)
+            textResult.text = "계산 결과: "+result
+        }
+        btnDiv.setOnClickListener {
+            num1 = edit1.text.toString()
+            num2 = edit2.text.toString()
+            result = Integer.parseInt(num1) / Integer.parseInt(num2)
+            textResult.text = "계산 결과: "+result
+        }
     }
 }
